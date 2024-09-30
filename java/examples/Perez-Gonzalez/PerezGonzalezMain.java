e11cfd43d3094b0335ab89406709fd63e7b41536
natjiang <nathan.jiang@outlook.com>
2024-09-30 12:08:28 -0500

    Add files via upload

--- java/examples/Perez-Gonzalez/PerezGonzalezMain.java
+++ java/examples/Perez-Gonzalez/PerezGonzalezMain.java
@@ -80,7 +80,7 @@ public class PerezGonzalezMain {
 
     private void execute() {
         Random rng = new Random(777);
-        MomentSpecification mySpecification = new PerezGonzalezSpecification("src/oroa1.csv");
+        MomentSpecification mySpecification = new PerezGonzalezSpecification("src/mb1.csv");
 
         double bestMinImprovement = 4.0;
         int bestMinObservationsPerLeaf = 25;
@@ -116,7 +116,7 @@ public class PerezGonzalezMain {
                 System.out.println("************************");
             }
             ArrayList<computeFitStatistics> cvList = new ArrayList<>();
-            for (int minObservationsPerLeaf = 20; minObservationsPerLeaf <= 200; minObservationsPerLeaf *= 2) {
+            for (int minObservationsPerLeaf = 10; minObservationsPerLeaf <= 200; minObservationsPerLeaf *= 2) {
                 for (double minImprovement = 0.001; minImprovement <= 10; minImprovement *= 2) {
                     for (int maxDepth = 1; maxDepth <= 7; maxDepth++) {
                         cvList.add(new computeFitStatistics(mySpecification, numberTreesInForest, rngBaseSeedMomentForest, verbose, minObservationsPerLeaf, minImprovement, maxDepth, rngBaseSeedOutOfSample, false));
