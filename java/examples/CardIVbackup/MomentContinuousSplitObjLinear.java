bb196b573d6876c58f0e221bb546737cd2dcfb9b
natjiang <nathan.jiang@outlook.com>
2024-10-31 09:35:35 -0500

    Add files via upload

---
+++ java/examples/CardIVbackup/MomentContinuousSplitObjLinear.java
@@ -0,0 +1,114 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 Stephen P. Ryan <stephen.p.ryan@wustl.edu>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package examples.CardIVbackup;
+
+import core.MomentContinuousSplitObj;
+import core.DataLens;
+import core.MomentSpecification;
+import core.SplitContainer;
+import optimization.Uncmin_methods;
+
+/**
+ *
+ * @author Stephen P. Ryan <stephen.p.ryan@wustl.edu>
+ */
+public class MomentContinuousSplitObjLinear extends MomentContinuousSplitObj {
+
+    SplitContainer container; 
+    DataLens lens;
+    int minCount;
+    double minProportion;
+    boolean debugVerbose = false;
+    MomentSpecification spec;
+
+    public MomentContinuousSplitObjLinear(int indexSplitVariable, DataLens lens, double minProportion, int minCount, MomentSpecification spec) {
+        this.indexSplitVariable = indexSplitVariable;
+        this.lens = lens;
+        this.minCount = minCount;
+        this.minProportion = minProportion;
+        this.spec = spec;
+
+        // System.out.println("MomentContinuousSplitObjBart.java:26 -> min/max x_1 = "+pmUtility.min(X, 1)+" "+pmUtility.max(X,1));
+    }
+
+    @Override
+    public int getEffectiveNumObsLeft() {
+        return numObsLeft;
+    }
+
+    @Override
+    public int getEffectiveNumObsRight() {
+        return numObsRight;
+    }
+
+    @Override
+    public double getSSE() {
+
+        leftMSE = 0;
+        rightMSE = 0;
+
+        ContainerIVBackup leftLinear = new ContainerIVBackup(container.getLeft(), spec.getHomogeneousIndex(), spec.getHomogeneousParameterVector(), false); //This object will compute the beta and MSE for the left split
+        ContainerIVBackup rightLinear = new ContainerIVBackup(container.getRight(), spec.getHomogeneousIndex(), spec.getHomogeneousParameterVector(), false);
+        leftLinear.computeBetaAndErrors();
+        rightLinear.computeBetaAndErrors();
+
+        leftMSE = leftLinear.getGoodnessOfFit();
+        rightMSE = rightLinear.getGoodnessOfFit();
+
+//        if (leftLinear.getBeta() != null && rightLinear.getBeta() != null) {
+//            pmUtility.prettyPrintVector(leftLinear.getBeta());
+//            pmUtility.prettyPrintVector(rightLinear.getBeta());
+//        }
+        if (debugVerbose) {
+            System.out.println("MSE = " + (leftMSE + rightMSE) + " n_Left: " + numObsLeft + " n_Right: " + numObsRight + " MSE_Left: " + leftMSE + " MSE_Right: " + rightMSE + " minCount: " + minCount);
+        }
+
+        if (getEffectiveNumObsLeft() < minCount) {
+            if (debugVerbose) {
+                System.out.println("Not enough n_left = " + getEffectiveNumObsLeft() + " ==> Triggered positive infinity");
+            }
+            leftMSE = Double.POSITIVE_INFINITY;
+            // return Double.POSITIVE_INFINITY;
+        }
+        if (getEffectiveNumObsRight() < minCount) {
+            if (debugVerbose) {
+                System.out.println("Not enough n_right = " + getEffectiveNumObsRight() + " ==> Triggered positive infinity");
+            }
+            rightMSE = Double.POSITIVE_INFINITY;
+            // return Double.POSITIVE_INFINITY;
+        }
+
+        // return (leftMSE + rightMSE) / X.getNumObs();
+        return (leftMSE + rightMSE);
+    }
+
+    @Override
+    public double f_to_minimize(double splitPoint) {
+        container = SplitContainer.getContinuousDataSplit(lens, splitPoint, indexSplitVariable); //This returns the data split into each leaf based on splitpoint and index of split variable
+        numObsLeft = container.getLeft().getNumObs();
+        numObsRight = container.getRight().getNumObs();
+        return getSSE();
+    }
+
+}
